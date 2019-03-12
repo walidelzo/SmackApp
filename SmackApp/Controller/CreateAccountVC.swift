d211adb082b8731d40dba59dfe9e31ae94ff871e
Admin <walidelzo@gmail.com>
2019-03-12 15:18:53 +0200

    add register user and rounded button class

--- SmackApp/Controller/CreateAccountVC.swift
+++ SmackApp/Controller/CreateAccountVC.swift
@@ -9,6 +9,18 @@
 import UIKit
 
 class CreateAccountVC: UIViewController {
+    
+    //OutLets
+    
+    @IBOutlet weak var userNameTxt: UITextField!
+    
+    @IBOutlet weak var emailTxt: UITextField!
+    
+    @IBOutlet weak var passWordTxt: UITextField!
+    
+    @IBOutlet weak var avatarImageView: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -16,6 +28,27 @@ class CreateAccountVC: UIViewController {
     }
     
     
+    
+    // @ibAction
+    
+    @IBAction func signUpPressed(_ sender: Any) {
+        guard let email = emailTxt.text , emailTxt.text != ""   else { return }
+        guard let pass = passWordTxt.text , passWordTxt.text != "" else { return  }
+        
+        AuthService.instance.registerUser(withEmailAddress: email, AndPassWord: pass) { (Success ) in
+            if (Success){
+                print("the user is registerd")
+            }
+        }
+        
+        
+    }
+    
+    @IBAction func geratecolorPressed(_ sender: Any) {
+    }
+    
+    @IBAction func pickAvatarPressed(_ sender: Any) {
+    }
     @IBAction func closeActionPressed(_ sender: Any) {
         performSegue(withIdentifier: TO_WIND_TO_CHANNEL_SEGUE, sender: nil)
         
---
+++ SmackApp/View/RoundedButton.swift
@@ -0,0 +1,34 @@
+//
+//  RoundedButton.swift
+//  SmackApp
+//
+//  Created by Admin on 3/12/19.
+//  Copyright © 2019 NanoSoft. All rights reserved.
+//
+
+import UIKit
+@IBDesignable
+class RoundedButton: UIButton {
+    
+    @IBInspectable var radiusValue : CGFloat = 3.0
+        {
+        
+        didSet{  self.setNeedsLayout()    }
+    }
+    
+    override func awakeFromNib() {
+        self.setUp()
+    }
+    
+    
+    func setUp(){
+        self.layer.cornerRadius = radiusValue
+    }
+    
+    override func prepareForInterfaceBuilder() {
+        super.prepareForInterfaceBuilder()
+        self.setUp()
+    }
+    
+    
+}
