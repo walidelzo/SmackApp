3db50b8444d76a2c6bd4c4dcdbadf8436c26ee9d
Admin <walidelzo@gmail.com>
2019-03-19 15:41:14 +0200

    add messages tableview and load messages

--- SmackApp/Controller/ChatVC.swift
+++ SmackApp/Controller/ChatVC.swift
@@ -8,13 +8,13 @@
 
 import UIKit
 
-class ChatVC: UIViewController {
+class ChatVC: UIViewController ,UITableViewDelegate,UITableViewDataSource {
     
     //MARK:- IBoutlets
     @IBOutlet var menuBtn: UIButton!
     @IBOutlet weak var channelLabel: UILabel!
     @IBOutlet weak var messageTXT: UITextField!
-    
+    @IBOutlet weak var tableview:UITableView!
     //MARK:- IBACTIONS
     @IBAction func sendBtnPressed(_ sender: Any) {
     
@@ -38,6 +38,12 @@ class ChatVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.bindToKeyboard()
+        
+        tableview.delegate = self
+        tableview.dataSource = self
+        tableview.estimatedRowHeight = 80
+        //tableview.rowHeight = automaticDimension
+        
         // add revealaction to menu button
         menuBtn.addTarget(self.revealViewController(), action: #selector(SWRevealViewController.revealToggle(_:)), for: .touchUpInside)
         
@@ -110,20 +116,33 @@ class ChatVC: UIViewController {
     func getMessages(){
         guard let  channelID = MassegeDataService.instance.selectedChannel?.id else { return }
         MassegeDataService.instance.findAllMessagesForChannel(channelId: channelID) { (succ) in
+            if succ {
+                self.tableview.reloadData()
+            }
 
         }
     }
     
-//    func sendMessage(){
-//        let userId = UserDataService.instance.id
-//        let channelId = "5c8d22fc864c3c0574b70258"
-//        let username = UserDataService.instance.name
-//        let avatarname = UserDataService.instance.avatarName
-//        let avatarColor = UserDataService.instance.avatarColor
-//
-//        SocketService.instance.socketClient.emit("newMessage","Hello objective-c", userId,channelId,username,avatarname,avatarColor)
-//        print("the message was sent ......> .....")
-//    }
+    //MARK:- TableView Methods
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return MassegeDataService.instance.messages.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if let cell = tableView.dequeueReusableCell(withIdentifier: "messageCell", for: indexPath) as? MessageCell
+        {
+            let messageOne = MassegeDataService.instance.messages[indexPath.row]
+            cell.configureCell(message: messageOne)
+            return cell 
+            
+        }else{
+            return MessageCell()
+        }
+    }
     
     
 }
---
+++ SmackApp/View/MessageCell.swift
@@ -0,0 +1,35 @@
+//
+//  MessageCell.swift
+//  SmackApp
+//
+//  Created by Admin on 3/19/19.
+//  Copyright © 2019 NanoSoft. All rights reserved.
+//
+
+import UIKit
+
+class MessageCell: UITableViewCell {
+    //MARK:- Oulets
+    
+    @IBOutlet weak var imageMessage: UIImageView!
+    @IBOutlet weak var userNameLbl: UILabel!
+    @IBOutlet weak var timeStampLbl: UILabel!
+    @IBOutlet weak var MessageLbl: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    func configureCell (message:Message){
+        imageMessage.backgroundColor =  UserDataService.instance.returnColor(avatarColorString: message.userAvatarColor)
+        imageMessage.image = UIImage(named: message.userAvatar)
+        userNameLbl.text = message.userName
+        timeStampLbl.text = message.timeStamp
+        MessageLbl.text = message.message
+    }
+
+   
+
+}
